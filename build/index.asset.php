1b5b2642dafab21e5193158f7c58035cffb38be8
Nawras <nawras.othman@yandex.com>
2025-08-13 10:49:05 +0300

    new build after updating packages

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '2745c18354af46e12e48');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '3736e117d23b0f601047');
