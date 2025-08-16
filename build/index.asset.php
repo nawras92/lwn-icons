5f3589981d62a75348e2fa1e810952098c4cba02
Nawras <nawras.othman@yandex.com>
2025-08-16 12:35:12 +0300

    change icon list style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '88e6475be8f2ecbcda2f');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ece66011bca1ac94c550');
